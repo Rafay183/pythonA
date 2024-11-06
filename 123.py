4673a6bdd807f805ce051690632fc9f3b753f865
rafay183 <rafayshaikh633@gmail.com>
2024-11-06 10:48:43 +0500

    first commit

---
+++ 123.py
@@ -0,0 +1,11 @@
+a=10
+b=15
+if a>=b and a==b:
+    print("A is less than b")    
+elif a<=b and a!=b:
+    print("A is not equal to b")
+elif a<b and b>a:
+    print("B is greater than a")  
+else:
+    print("syntax error")
+    
